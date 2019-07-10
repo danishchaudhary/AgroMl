3775a436b537a934c696cf53725bf4fc3820c738
abhinavshukla021 <48839042+abhinavshukla021@users.noreply.github.com>
2019-07-10 12:40:23 +0530

    Fuck

--- predict.py
+++ predict.py
@@ -29,8 +29,8 @@ def predict(Crop, Stage, l):
 
         else:
             for var in l:
-            temp.insert(i,chilli_all_other.predict_proba(np.asarray(var).reshape(1,-1)))
-            i=i+1
+                temp.insert(i,chilli_all_other.predict_proba(np.asarray(var).reshape(1,-1)))
+                i=i+1
             temp1=[]
             i=0
             for var in temp:
@@ -57,8 +57,8 @@ def predict(Crop, Stage, l):
 
         else:
             for var in l:
-            temp.insert(i,tomato_all_other.predict_proba(np.asarray(var).reshape(1,-1)))
-            i=i+1
+                temp.insert(i,tomato_all_other.predict_proba(np.asarray(var).reshape(1,-1)))
+                i=i+1
             temp1=[]
             i=0
             for var in temp:
@@ -66,4 +66,45 @@ def predict(Crop, Stage, l):
               var=[0]+var
               temp1.insert(i,var)
               i=i+1
+            return (temp1)
+        
+    if(Crop=='Cotton'):
+        if(Stage=='Seedling'):
+            for var in l:
+                temp.insert(i,cotton_seedling.predict_proba(np.asarray(var).reshape(1,-1)))
+                i=i+1
+            temp1=[]
+            i=0
+            for var in temp:
+              var=var[0].tolist()
+              var=[var[0],var[1]]+[0]+var[2:]
+              temp1.insert(i,var)
+              i=i+1
+            return (temp1)
+
+        elif (Stage=='Flowering'):
+            for var in l:
+                temp.insert(i,cotton_flowering.predict_proba(np.asarray(var).reshape(1,-1)))
+                i=i+1
+                temp1=[]    
+                i=0
+                for var in temp:
+                  var=var[0].tolist()
+                  var=[var[0]]+[0]+var[1:]
+                  temp1.insert(i,var)
+                  i=i+1
+                temp1  
+            return (temp1)
+
+        else:
+            for var in l:
+            temp.insert(i,cotton_all_other.predict_proba(np.asarray(var).reshape(1,-1)))
+            i=i+1
+            temp1=[]    
+            i=0
+            for var in temp:
+              var=var[0].tolist()
+              var=[var[0]]+[0]+[var[1]]+[0]+[var[2]]
+              temp1.insert(i,var)
+              i=i+1  
             return (temp1)
